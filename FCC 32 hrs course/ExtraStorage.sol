fbfe29421f18bc6bc894cd5c2edd769e2816f7a4
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 14:35:44 +0100

    override sore function

--- FCC 32 hrs course/ExtraStorage.sol
+++ FCC 32 hrs course/ExtraStorage.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 // ExtraStorage inherits all the public and external functions of SimpleStorage
-contract ExtraStorage is SimpleStorage {}
+contract ExtraStorage is SimpleStorage {
+    // override store function
+    function store(uint256 _number) public override {
+        number = _number + 5;
+    }
+}
