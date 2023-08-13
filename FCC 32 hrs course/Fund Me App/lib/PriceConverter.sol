52436417b9d5ae833421f01c799e4b10ee2b1eec
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 21:03:13 +0100

    create a library

---
+++ FCC 32 hrs course/Fund Me App/lib/PriceConverter.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// libraries can;t have state variables and also can't send ether and all library functions will be internal
+library PriceConverter{}
