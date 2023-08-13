f0754b90ca1c5daa180a2e050aa842d4d2943214
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 21:06:59 +0100

    use the library

--- FCC 32 hrs course/Fund Me App/FundMeClean.sol
+++ FCC 32 hrs course/Fund Me App/FundMeClean.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./lib/PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUSD = 50 * 1e18;
 
     address[] public funders;
@@ -17,25 +19,6 @@ contract FundMe {
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
-    }
-
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-
-        (, int answer, , , ) = priceFeed.latestRoundData();
-
-        return uint256(answer * 1e10);
-    }
-
-    function getConversionRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-
-        return ethAmountInUsd;
     }
 
     function withdraw() internal {}
