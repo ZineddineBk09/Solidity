a93886986bee57c407d1ea52447886dd27ae4097
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 14:46:50 +0100

    create a fund me app where people can fund and withdraw ETH

--- FCC 32 hrs course/Fund Me App/FundMe.sol
+++ FCC 32 hrs course/Fund Me App/FundMe.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract FundMe {}
+contract FundMe {
+    function fund() public {
+      // things to take in consideration:
+      // 1. set a minimum amount of money to fund in USD
+      // 2. how to send ETH to this contract
+    }
+
+    function withdraw() internal {}
+}
