2ad786fc6a45ce0627308eb0f7cd302b0caacb96
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 11:47:08 +0100

    write a contract "StorageFactory" to deploy other contracts

--- FCC 32 hrs course/StorageFctory.sol
+++ FCC 32 hrs course/StorageFctory.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 contract StorageFacotry {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorage;
 
     // the below function will help to deploy the contract './SimpleStorage.sol'
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorage = new SimpleStorage();
+        simpleStorage.push(newSimpleStorage);
     }
 }
