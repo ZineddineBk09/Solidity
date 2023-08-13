377239d5c95f36711663ab9e8fddafceeff7de96
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 14:30:17 +0100

    inheritance

---
+++ FCC 32 hrs course/ExtraStorage.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./SimpleStorage.sol";
+
+// ExtraStorage inherits all the public and external functions of SimpleStorage
+contract ExtraStorage is SimpleStorage {}
