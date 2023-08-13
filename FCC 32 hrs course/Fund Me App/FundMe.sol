e697dd118d50394b2a1e38eaddb202381655d9e6
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 14:55:04 +0100

    make function payable to make it red insteas of orange in remix, and to make it capable of holding funds

--- FCC 32 hrs course/Fund Me App/FundMe.sol
+++ FCC 32 hrs course/Fund Me App/FundMe.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// NOTE: contract can hold funds same as a wallet the only difference is that a contract can hold funds and do something with it (e.g. send it to another address)  
+// NOTE: contract can hold funds same as a wallet the only difference is that a contract can hold funds and do something with it (e.g. send it to another address) , because they an address
 contract FundMe {
-    function fund() public {
+    function fund() public payable {
         // things to take in consideration:
         // 1. set a minimum amount of money to fund in USD
         // 2. how to send ETH to this contract
+
+        // msg is a global variable that is available to us in solidity to get information about the transaction
+        require(msg.value > 1e18);
     }
 
     function withdraw() internal {}
