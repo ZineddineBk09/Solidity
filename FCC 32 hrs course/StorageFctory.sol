090720b8afa681a683f61d4458e818c92b26dcd1
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 11:46:19 +0100

    write a contract "StorageFactory" to deploy other contracts

--- FCC 32 hrs course/StorageFctory.sol
+++ FCC 32 hrs course/StorageFctory.sol
@@ -1,37 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract SimpleStorage {
-    uint256 number;
-
-    People public zino = People({id: 1, name: "zineddine"});
-
-    People[] public people;
-
-    struct People {
-        uint256 id;
-        string name;
-    }
-
-    mapping(string => uint256) public nameToIdMapping;
-
-    function store(uint256 _number) public {
-        number = _number;
-    }
-
-    function getNumber() public view returns (uint256) {
-        return number;
-    }
-
-    function addPerson(string memory _name, uint256 _id) public {
-        People memory newPerson = People(_id, _name);
-        people.push(newPerson);
-
-        nameToIdMapping[_name] = _id;
-    }
-}
+import "./SimpleStorage.sol";
 
 contract StorageFacotry {
+    SimpleStorage public simpleStorage;
+
     // the below function will help to deploy the contract './SimpleStorage.sol'
-    function createSimpleStorageContract() public {}
+    function createSimpleStorageContract() public {
+        simpleStorage = new SimpleStorage();
+    }
 }
