99a1350349452151d0403ed568a4870ca36f7414
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 14:52:19 +0100

    create a fund me app where people can fund and withdraw ETH

--- FCC 32 hrs course/Fund Me App/FundMe.sol
+++ FCC 32 hrs course/Fund Me App/FundMe.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// NOTE: contract can hold funds same as a wallet the only difference is that a contract can hold funds and do something with it (e.g. send it to another address)  
 contract FundMe {
     function fund() public {
-      // things to take in consideration:
-      // 1. set a minimum amount of money to fund in USD
-      // 2. how to send ETH to this contract
+        // things to take in consideration:
+        // 1. set a minimum amount of money to fund in USD
+        // 2. how to send ETH to this contract
     }
 
     function withdraw() internal {}
