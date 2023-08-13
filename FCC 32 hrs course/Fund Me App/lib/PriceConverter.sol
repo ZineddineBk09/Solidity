93f8753911b0fb29e8d1593d1f43356d035a3cdc
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 21:05:03 +0100

    create a library

--- FCC 32 hrs course/Fund Me App/lib/PriceConverter.sol
+++ FCC 32 hrs course/Fund Me App/lib/PriceConverter.sol
@@ -1,5 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 // libraries can;t have state variables and also can't send ether and all library functions will be internal
-library PriceConverter{}
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+
+        (, int answer, , , ) = priceFeed.latestRoundData();
+
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
+    }
+}
