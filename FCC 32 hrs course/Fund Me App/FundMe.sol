942a5a3b22f84aa5996deba51382737e6fbf9866
ZineddineBk09 <eino1209@gmail.com>
2023-08-13 17:32:13 +0100

    use Sepolia testnet price feeds

--- FCC 32 hrs course/Fund Me App/FundMe.sol
+++ FCC 32 hrs course/Fund Me App/FundMe.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 // NOTE: contract can hold funds same as a wallet the only difference is that a contract can hold funds and do something with it (e.g. send it to another address) , because they an address
 contract FundMe {
+    uint256 public minimumUSD = 50;
+
     function fund() public payable {
         // things to take in consideration:
         // 1. set a minimum amount of money to fund in USD
@@ -12,8 +14,16 @@ contract FundMe {
         // msg.value use wei as unit ==> 1e18 wei = 1 ETH
         // if the value is less than 1 ETH then the transaction will revert and the ETH will be sent back to the sender and everything done in the transaction (fund function) will be reverted
         // BUT the gas will be spent anyway
-        require(msg.value > 1e18, "You need to spend more ETH");
+        require(msg.value > minimumUSD, "You need to spend more ETH");
     }
+
+    // get the ETH/USD price feed
+    function getPrice() public {
+        // we need the ABI + Address of the contract that we want to interact with
+        // address: 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    }
+
+    function getConversionRate() public {}
 
     function withdraw() internal {}
 }
