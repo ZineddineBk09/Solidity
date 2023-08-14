2da8bfe73245470b2800221c0c6d62cc070908b4
ZineddineBk09 <eino1209@gmail.com>
2023-08-14 08:34:30 +0100

    use the library

--- FCC 32 hrs course/Fund Me App/FundMe.sol
+++ FCC 32 hrs course/Fund Me App/FundMe.sol
@@ -15,6 +15,9 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    // NOTE: this is a special function that is called when the contract is deployed
+    constructor() {}
+
     function fund() public payable {
         // things to take in consideration:
         // 1. set a minimum amount of money to fund in USD
@@ -25,36 +28,12 @@ contract FundMe {
         // if the value is less than 1 ETH then the transaction will revert and the ETH will be sent back to the sender and everything done in the transaction (fund function) will be reverted
         // BUT the gas will be spent anyway
         require(
-            getConversionRate(msg.value) >= minimumUSD,
+            (msg.value.getConversionRate()) >= minimumUSD,
             "You need to spend more ETH"
         );
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
-    }
-
-    // get the ETH/USD price feed
-    function getPrice() public view returns (uint256) {
-        // we need the ABI + Address of the contract that we want to interact with
-        // address: 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-
-        (, int answer, , , ) = priceFeed.latestRoundData();
-
-        // 1 ETH = 10^18 wei
-        // the answer will have 8 decimals so we need to multiply it by 10^10 to get the price in wei
-        return uint256(answer * 1e10);
-    }
-
-    function getConversionRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-
-        return ethAmountInUsd;
     }
 
     // withdraw all the funds
