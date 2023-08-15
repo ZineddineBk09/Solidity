de5b7997e5666a295d19974eaa809f7c7e1d015f
ZineddineBk09 <eino1209@gmail.com>
2023-08-15 11:46:19 +0100

    deploy SimpleStorage with few lines of code locally using Sepolia network

--- FCC 32 hrs course/hard hat/hard hat simple storage /hardhat.config.ts
+++ FCC 32 hrs course/hard hat/hard hat simple storage /hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import 'dotenv/config'
+
+const RPC_ALCHEMY_SEPOLIA_SERVER_URL =
+  process.env.RPC_ALCHEMY_SEPOLIA_SERVER_URL!
+const METAMASK_ACCOUNT_PRIVATE_KEY = process.env.METAMASK_ACCOUNT_PRIVATE_KEY!
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    sepolia: {
+      url: RPC_ALCHEMY_SEPOLIA_SERVER_URL,
+      accounts: [METAMASK_ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  solidity: '0.8.19',
+}
 
-export default config;
+export default config
