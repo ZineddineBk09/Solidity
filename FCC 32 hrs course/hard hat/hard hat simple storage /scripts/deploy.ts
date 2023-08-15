e0a7218b3a1ac2421ec18ba81860a4cb4afbb6d5
ZineddineBk09 <eino1209@gmail.com>
2023-08-15 11:38:14 +0100

    deploy SimpleStorage with few lines of code locally using HardHat network

--- FCC 32 hrs course/hard hat/hard hat simple storage /scripts/deploy.ts
+++ FCC 32 hrs course/hard hat/hard hat simple storage /scripts/deploy.ts
@@ -1,6 +1,12 @@
 import { ethers } from 'hardhat'
 
-async function main() {}
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
+  console.log('Deploying SimpleStorage...')
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  const address = await simpleStorage.getAddress()
+  console.log('SimpleStorage deployed to:', address)
+}
 
 main().catch((error) => {
   console.error(error)
