7f760ef881e82ebefc301a63ebeee20edcec5240
ZineddineBk09 <eino1209@gmail.com>
2023-08-15 17:29:22 +0100

    create a block number task

--- FCC 32 hrs course/hard hat/hard hat simple storage /tasks/block-number.ts
+++ FCC 32 hrs course/hard hat/hard hat simple storage /tasks/block-number.ts
@@ -2,5 +2,11 @@ import { task } from 'hardhat/config'
 
 // task syntax: task(name, [description], asyncFn)
 task('block-number', 'Prints the current block number').setAction(
-  async (taskArgs: any, hre) => {}
+  async (taskArgs: any, hre) => {
+    // taskArgs: an object with the arguments passed to the task via the CLI.
+    // hre: the Hardhat Runtime Environment. when running a task, you have access to all the plugins and your Hardhat config.
+    await hre.ethers.provider.getBlockNumber().then((blockNumber: number) => {
+      console.log('Current block number: ' + blockNumber)
+    })
+  }
 )
