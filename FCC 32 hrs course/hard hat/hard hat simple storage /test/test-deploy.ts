0a8579d4cbf4881ce721bc8624c85ec50fce3416
ZineddineBk09 <eino1209@gmail.com>
2023-08-15 18:00:28 +0100

    write deploy test

--- FCC 32 hrs course/hard hat/hard hat simple storage /test/test-deploy.ts
+++ FCC 32 hrs course/hard hat/hard hat simple storage /test/test-deploy.ts
@@ -1,13 +1,17 @@
 import { ethers, run, network } from 'hardhat'
+import { expect } from 'chai'
 
 describe('SimpleStorage', () => {
+  let SimpleStorageFactory: any
   let simpleStorage: any
 
   beforeEach(async () => {
-    simpleStorage = await ethers
-      .getContractFactory('SimpleStorage')
-      .then((factory) => {
-        return factory.deploy()
-      })
+    SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
+    simpleStorage = await SimpleStorageFactory.deploy()
+  })
+
+  it('should start with number = 0', async () => {
+    const number = await simpleStorage.getNumber()
+    expect(number).to.equal('0')
   })
 })
