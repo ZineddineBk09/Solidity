90e6c11e9c4e72ea420bd762431f0d37169c971c
ZineddineBk09 <eino1209@gmail.com>
2023-08-15 18:25:19 +0100

    add solidity-coverage package to look for untedted lines of code of our contracts and scripts

--- FCC 32 hrs course/hard hat/hard hat simple storage /hardhat.config.ts
+++ FCC 32 hrs course/hard hat/hard hat simple storage /hardhat.config.ts
@@ -4,6 +4,8 @@ import 'dotenv/config'
 import '@nomicfoundation/hardhat-verify'
 import './tasks/block-number'
 import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+
 
 const RPC_ALCHEMY_SEPOLIA_SERVER_URL =
   process.env.RPC_ALCHEMY_SEPOLIA_SERVER_URL || ''
@@ -23,7 +25,7 @@ const config: HardhatUserConfig = {
     localhost: {
       url: 'http://127.0.0.1:8545/',
       chainId: 31337,
-      // accounts: hardhat has already 20 accounts with 10k eth,
+      // accounts: hardhat has already set up 20 accounts with 10k eth,
     },
   },
   etherscan: {
