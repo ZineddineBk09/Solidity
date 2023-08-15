e2fba6369b8d8a4d53373f8d96fc1cf03b0594c0
ZineddineBk09 <eino1209@gmail.com>
2023-08-15 18:05:10 +0100

    write deploy test

--- FCC 32 hrs course/hard hat/hard hat simple storage /test/test-deploy.ts
+++ FCC 32 hrs course/hard hat/hard hat simple storage /test/test-deploy.ts
@@ -14,4 +14,12 @@ describe('SimpleStorage', () => {
     const number = await simpleStorage.getNumber()
     expect(number).to.equal('0')
   })
+
+  it('should set number to 1 when we call store', async () => {
+    const txResponse = await simpleStorage.store(1)
+    const txReceipt = await txResponse.wait(1)
+    const currentNumber = await simpleStorage.getNumber()
+
+    expect(currentNumber).to.equal('1')
+  })
 })
