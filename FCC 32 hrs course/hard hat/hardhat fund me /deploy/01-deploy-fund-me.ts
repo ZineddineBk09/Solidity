fc1bc2d9de20ced0e44e166c111dedae899032de
ZineddineBk09 <eino1209@gmail.com>
2023-08-22 10:51:19 +0100

    writing the deploy function for fund me contract

--- FCC 32 hrs course/hard hat/hardhat fund me /deploy/01-deploy-fund-me.ts
+++ FCC 32 hrs course/hard hat/hardhat fund me /deploy/01-deploy-fund-me.ts
@@ -1,1 +1,7 @@
-// 
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+export default async function deployFunc(hre: HardhatRuntimeEnvironment) {
+  console.log('Deploying fund me contract... 🚀')
+
+  const { deployments, getNamedAccounts } = hre
+}
