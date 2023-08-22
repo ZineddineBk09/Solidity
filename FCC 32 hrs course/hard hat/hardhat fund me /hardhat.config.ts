ce18fb3d1eab82c01878fe4a44eaec7fd2f28133
ZineddineBk09 <eino1209@gmail.com>
2023-08-22 10:23:54 +0100

    add some tasks

--- FCC 32 hrs course/hard hat/hardhat fund me /hardhat.config.ts
+++ FCC 32 hrs course/hard hat/hardhat fund me /hardhat.config.ts
@@ -1,8 +1,54 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+require('@nomiclabs/hardhat-waffle')
+require('hardhat-gas-reporter')
+require('@nomiclabs/hardhat-etherscan')
+require('dotenv').config()
+require('solidity-coverage')
+require('hardhat-deploy')
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ''
+const SEPOLIA_RPC_URL = process.env.RPC_ALCHEMY_SEPOLIA_SERVER_URL || ''
+const PRIVATE_KEY = process.env.METAMASK_ACCOUNT_PRIVATE_KEY || ''
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.7',
+      },
+      {
+        version: '0.6.6',
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  mocha: {
+    timeout: 500000,
+  },
+}
 
-export default config;
+export default config
