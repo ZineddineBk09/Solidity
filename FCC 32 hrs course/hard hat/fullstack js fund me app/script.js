7b9eae29db84f8d2c95bcf38bc73958893fafe87
ZineddineBk09 <eino1209@gmail.com>
2023-09-06 12:02:46 +0100

    transform ethers to logic separate file

---
+++ FCC 32 hrs course/hard hat/fullstack js fund me app/script.js
@@ -0,0 +1,41 @@
+import { ethers } from 'https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js'
+
+const connectBtn = document.getElementById('connect-button')
+const fundBtn = document.getElementById('fund-button')
+
+connectBtn.onclick = connect
+fundBtn.onclick = fund(0.01)
+
+async function connect() {
+  console.log('connecting to metamask')
+  if (typeof window.ethereum !== 'undefined') {
+    // after the page loads, ask the user to connect their metamask wallet if they approve we can then send requests to their wallet
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+    console.log('connected to metamask')
+
+    connectBtn.innerHTML =
+      'Connected' +
+      '<br>' +
+      window.ethereum.selectedAddress.slice(0, 6) +
+      '...' +
+      window.ethereum.selectedAddress.slice(-4)
+  } else {
+    // if the user doesn't have metamask installed, show them a message to install it
+    alert('Please install MetaMask to use this dApp!')
+  }
+}
+
+async function fund(ethAmount) {
+  console.log('funding project with ' + ethAmount + ' ETH')
+  if (typeof window.ethereum !== 'undefined') {
+    // To send a transaction we need:
+    // 1. connection to the blockchain
+    // 2. a wallet with ETH in it
+    // 3. a smart contract address that we want to interact with ==> ABI + address
+    // provider is a connection to the blockchain
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    // signer is a wallet with ETH in it (in this case the user's metamask wallet)
+    const signer = provider.getSingner()
+    console.log('wallet: ', signer)
+  }
+}
