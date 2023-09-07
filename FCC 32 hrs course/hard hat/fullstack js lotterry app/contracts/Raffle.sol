2babb1562c6b1bbddb2397d2807731d3c26a9ec7
ZineddineBk09 <eino1209@gmail.com>
2023-09-07 12:01:50 +0100

    add functions:  enterRaffle,  getEntraceFee,  getPlayer

---
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/contracts/Raffle.sol
@@ -0,0 +1,31 @@
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+    // State variables
+    uint256 private immutable i_entraceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 _entranceFee) {
+        i_entraceFee = _entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entraceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+
+        s_players.push(payable(msg.sender));
+    }
+
+    function getEntraceFee() public view returns (uint256) {
+        return i_entraceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
