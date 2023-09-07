e128aa3c0416fac0902aea6423f16b552a739c42
ZineddineBk09 <eino1209@gmail.com>
2023-09-07 22:05:37 +0100

    add named accounts for raffle deployer

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/hardhat.config.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/hardhat.config.js
@@ -9,4 +9,12 @@ require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.19',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 }
