0a70dbc818e52da4de291121f3e87d259d0883db
ZineddineBk09 <eino1209@gmail.com>
2023-09-07 22:10:12 +0100

    raffle contracts deploy script

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/deploy/01-deploy-raffle.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/deploy/01-deploy-raffle.js
@@ -1,4 +1,11 @@
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+
+  const raffle = await deploy('Raffle', {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: 6,
+  })
 }
