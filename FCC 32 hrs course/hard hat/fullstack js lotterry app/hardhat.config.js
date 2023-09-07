087a6e31b82de439af4618f424fab5944b88b3d2
ZineddineBk09 <eino1209@gmail.com>
2023-09-07 11:47:43 +0100

    add plugins

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/hardhat.config.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/hardhat.config.js
@@ -1,4 +1,13 @@
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
+import 'hardhat-deploy'
+import 'hardhat-deploy-ethers'
+import 'hardhat-gas-reporter'
+import 'hardhat-contract-sizer'
+import 'solidity-coverage'
+import 'dotenv/config'
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.19',
+}
