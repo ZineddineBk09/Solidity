ef0e2fbe8fb0d0fcde2217542b1b77052f22760e
ZineddineBk09 <eino1209@gmail.com>
2023-09-08 08:07:52 +0100

    fix

---
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+  11155111: {
+    name: 'sepolia',
+    vrfCoordinatorV2: '0x271682DEB8C4E0901D1a1550aD2e64D568E69909',
+  },
+}
+
+const developmentChains = ['hardhat', 'localhost']
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+}
