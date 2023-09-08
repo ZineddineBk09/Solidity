4a9b9d2657de45167f14ba9adcd27c8b9edd9fe7
ZineddineBk09 <eino1209@gmail.com>
2023-09-08 09:58:13 +0100

    add hardhat network

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
@@ -1,10 +1,17 @@
 // This is just a helper file to help you get started with hardhat-deploy based on the network you're deploying to. You can use this file as a reference to help you get started with your own hardhat-deploy config file.
 // In our case we are deploying to the sepolia network, so we need to add the following code to the bottom of the file:
 
+const { ethers } = require('hardhat')
+
 const networkConfig = {
   11155111: {
     name: 'sepolia',
     vrfCoordinatorV2: '0x271682DEB8C4E0901D1a1550aD2e64D568E69909',
+    entraceFee: ethers.utils.parseEther('0.01'),
+  },
+  31337: {
+    name: 'hardhat',
+    entraceFee: ethers.utils.parseEther('0.01'),
   },
 }
 
