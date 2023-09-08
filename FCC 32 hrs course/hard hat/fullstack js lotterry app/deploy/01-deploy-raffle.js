471b87eed30df7f40101ad3838dea4a5f09e7237
ZineddineBk09 <eino1209@gmail.com>
2023-09-08 10:04:09 +0100

    add gasLane which is the 200 gwei Key Hash	in sepolia testnet

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/deploy/01-deploy-raffle.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/deploy/01-deploy-raffle.js
@@ -4,7 +4,10 @@ import { developmentChains, networkConfig } from '../helper-hardhat-config'
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
   let vrfCoordinatorV2Address
+  const entranceFee = networkConfig[chainId]['entranceFee']
+  const gasLane = networkConfig[chainId]['gasLane']
 
   // check if we're in a local development environment
   if (developmentChains.includes(network.name)) {
@@ -13,13 +16,12 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     )
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
   } else {
-    vrfCoordinatorV2Address =
-      networkConfig[network.config.chainId]['vrfCoordinatorV2']
+    vrfCoordinatorV2Address = networkConfig[chainId]['vrfCoordinatorV2']
   }
 
   const raffle = await deploy('Raffle', {
     from: deployer,
-    args: [],
+    args: [vrfCoordinatorV2Address, entranceFee, gasLane],
     log: true,
     waitConfirmations: network.config.blockConfirmations || 1,
   })
--- FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
@@ -8,10 +8,14 @@ const networkConfig = {
     name: 'sepolia',
     vrfCoordinatorV2: '0x271682DEB8C4E0901D1a1550aD2e64D568E69909',
     entraceFee: ethers.utils.parseEther('0.01'),
+    gasLane:
+      '0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef',
   },
   31337: {
     name: 'hardhat',
     entraceFee: ethers.utils.parseEther('0.01'),
+    gasLane:
+      '0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef',
   },
 }
 
