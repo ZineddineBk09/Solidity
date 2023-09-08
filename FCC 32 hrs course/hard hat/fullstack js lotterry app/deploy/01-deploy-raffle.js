1c43cb4a00b59290c17c1c2541c650db41bfcca2
ZineddineBk09 <eino1209@gmail.com>
2023-09-08 08:54:50 +0100

    add vrf coordinator address

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/deploy/01-deploy-raffle.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/deploy/01-deploy-raffle.js
@@ -1,8 +1,17 @@
-const { network } = require('hardhat')
+const { network, ethers } = require('hardhat')
+import { developmentChains } from '../helper-hardhat-config'
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+  let vrfCoordinatorV2Address
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      'VRFCoordinatorV2Mock'
+    )
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+  }
 
   const raffle = await deploy('Raffle', {
     from: deployer,
