5c09f7b71a406558215bdb1a187a09ad5b9c42d3
ZineddineBk09 <eino1209@gmail.com>
2023-09-08 08:33:01 +0100

    utility of hardhat helper file

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
@@ -1,3 +1,6 @@
+// This is just a helper file to help you get started with hardhat-deploy based on the network you're deploying to. You can use this file as a reference to help you get started with your own hardhat-deploy config file.
+// In our case we are deploying to the sepolia network, so we need to add the following code to the bottom of the file:
+
 const networkConfig = {
   11155111: {
     name: 'sepolia',
