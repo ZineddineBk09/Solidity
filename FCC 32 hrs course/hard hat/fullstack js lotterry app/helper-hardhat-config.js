35dc606142015d0c1650f11b397ab00cedec9c10
ZineddineBk09 <eino1209@gmail.com>
2023-09-09 12:33:06 +0100

    fix

--- FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/helper-hardhat-config.js
@@ -4,29 +4,45 @@
 const { ethers } = require('hardhat')
 
 const networkConfig = {
-  11155111: {
-    name: 'sepolia',
-    vrfCoordinatorV2: '0x271682DEB8C4E0901D1a1550aD2e64D568E69909',
-    entraceFee: ethers.utils.parseEther('0.01'),
-    gasLane:
-      '0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef',
-    subscriptionId: '1',
-    callbackGasLimit: '500000',
-    interval:'30'
+  default: {
+    name: 'hardhat',
+    keepersUpdateInterval: '30',
   },
   31337: {
-    name: 'hardhat',
-    entraceFee: ethers.utils.parseEther('0.01'),
+    name: 'localhost',
+    subscriptionId: '588',
     gasLane:
-      '0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef',
-    callbackGasLimit: '500000',
-    interval:'30'
+      '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
+    keepersUpdateInterval: '30',
+    raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
+    callbackGasLimit: '500000', // 500,000 gas
+  },
+  11155111: {
+    name: 'sepolia',
+    subscriptionId: '6926',
+    gasLane:
+      '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c', // 30 gwei
+    keepersUpdateInterval: '30',
+    raffleEntranceFee: ethers.utils.parseEther('0.01'), // 0.01 ETH
+    callbackGasLimit: '500000', // 500,000 gas
+    vrfCoordinatorV2: '0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625',
+  },
+  1: {
+    name: 'mainnet',
+    keepersUpdateInterval: '30',
   },
 }
 
 const developmentChains = ['hardhat', 'localhost']
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile =
+  '../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json'
+const frontEndAbiFile = '../nextjs-smartcontract-lottery-fcc/constants/abi.json'
 
 module.exports = {
   networkConfig,
   developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+  frontEndContractsFile,
+  frontEndAbiFile,
 }
