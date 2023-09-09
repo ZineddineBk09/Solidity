dc8f7c0c4411d50ef007e9fd38665ba40fc18767
ZineddineBk09 <eino1209@gmail.com>
2023-09-09 09:51:09 +0100

    add verify raffle script

---
+++ FCC 32 hrs course/hard hat/fullstack js lotterry app/utils/verify.js
@@ -0,0 +1,18 @@
+const { run } = reauire('hardhat')
+
+const verify = async (contractAddress, args) => {
+  console.log('Verifying contract...⌛⌛')
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Already verified ✅✅ ')
+    } else {
+      console.log('Failed to verify ❌❌ ')
+      console.log(e)
+    }
+  }
+}
