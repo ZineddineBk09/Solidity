97f6108adbe341a2766aa88466214989f7d5bef6
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 09:23:12 +0100

    add constructor

--- FCC 32 hrs course/hard hat/hardhat-nft/contracts/BasicNFT.sol
+++ FCC 32 hrs course/hard hat/hardhat-nft/contracts/BasicNFT.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.7;
 
-contract BasicNFT {}
+contract BasicNFT is ERC721 {
+    constructor() ERC721("Hamizzy", "HAM") {}
+}
