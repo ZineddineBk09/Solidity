1dd8ee42ff41a2799d82ab733465e1ed419a264e
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 11:27:08 +0100

    add chailink VRF

--- FCC 32 hrs course/hard hat/hardhat-nft/contracts/RandomNFT.sol
+++ FCC 32 hrs course/hard hat/hardhat-nft/contracts/RandomNFT.sol
@@ -4,6 +4,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 contract RandomNFT {
     // when we mint an NFT, we will trigger chainlink VRF to get us a random number
     // using that number we will get a random NFT of #: Shiba Inu, Pug, St. Bernard
