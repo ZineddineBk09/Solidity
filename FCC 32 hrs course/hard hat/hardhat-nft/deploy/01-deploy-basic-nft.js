a20232a2eb0775730c2e28b14a891864c328d284
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 09:45:01 +0100

    deploy script for BasicNFT

---
+++ FCC 32 hrs course/hard hat/hardhat-nft/deploy/01-deploy-basic-nft.js
@@ -0,0 +1,31 @@
+const { network } = require('hardhat')
+const { developmentChains } = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  log(
+    '========================= Deploying Basic NFT ============================='
+  )
+
+  const args = []
+  const basicNFT = await deploy('BasicNFT', {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log('Verifying...⌛⌛')
+    await verify(basicNFT.address, args)
+  }
+  log(
+    '========================= Finish Deploying Basic NFT ============================='
+  )
+}
