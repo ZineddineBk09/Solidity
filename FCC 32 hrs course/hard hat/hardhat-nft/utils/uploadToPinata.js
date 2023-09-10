33ba80cae6b703a1dc8a6b42a1e6c9a3949f98d3
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 21:32:53 +0100

    finish the upload function using pinata

--- FCC 32 hrs course/hard hat/hardhat-nft/utils/uploadToPinata.js
+++ FCC 32 hrs course/hard hat/hardhat-nft/utils/uploadToPinata.js
@@ -1,6 +1,49 @@
 const pinataSDK = require('@pinata/sdk')
 const path = require('path')
+const fs = require('fs')
+require('dotenv').config()
 
-function storeImages(imagesFilePath) {
+const pinataApiKey = process.env.PINATA_API_KEY || ''
+const pinataApiSecret = process.env.PINATA_API_SECRET || ''
+const pinata = new pinataSDK(pinataApiKey, pinataApiSecret)
+
+async function storeImages(imagesFilePath) {
   const fullImagesPath = path.resolve(imagesFilePath)
+  const files = fs.readdirSync(fullImagesPath)
+
+  console.log('Uploading to Pinata IPFS...⬆️⬆️⬆️⬆️')
+
+  const uploadPromises = files.map(async (file) => {
+    const readableStreamForFile = fs.createReadStream(
+      fullImagesPath + '/' + file
+    )
+    const options = {
+      pinataMetadata: {
+        name: file,
+      },
+    }
+
+    try {
+      const res = await pinata.pinFileToIPFS(readableStreamForFile, options)
+      console.log('Uploaded:', file)
+      return res
+    } catch (err) {
+      console.error('Error uploading:', file, err)
+      throw err
+    }
+  })
+
+  try {
+    const responses = await Promise.all(uploadPromises)
+    console.log('Files Uploaded Successfully ✅✅')
+    console.log({ responses, files })
+    return { responses, files }
+  } catch (error) {
+    console.error('Error uploading files:', error)
+    return { responses: [], files }
+  }
+}
+
+module.exports = {
+  storeImages,
 }
