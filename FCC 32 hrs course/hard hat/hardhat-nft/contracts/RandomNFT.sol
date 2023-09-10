ae27974440cdf4b3bbabe8730f6b0490c115b645
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 11:40:00 +0100

    add constructor and requestNFT function

--- FCC 32 hrs course/hard hat/hardhat-nft/contracts/RandomNFT.sol
+++ FCC 32 hrs course/hard hat/hardhat-nft/contracts/RandomNFT.sol
@@ -4,8 +4,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 contract RandomNFT {
     // when we mint an NFT, we will trigger chainlink VRF to get us a random number
@@ -14,8 +15,34 @@ contract RandomNFT {
 
     // users have to pay to mint an NFT
     // the owner of the contract can withdraw the ETH
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinatorV2;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3; // 3 is the minimum
+    uint32 private constant NUM_WORDS = 1; // number of random values returned
 
-    function requestNFT() public {}
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinatorV2 = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function requestNFT() public returns (uint256 requestId) {
+        requestId = i_vrfCoordinatorV2.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
 
     function fullfillRandomWords(
         uint256 requestId,
