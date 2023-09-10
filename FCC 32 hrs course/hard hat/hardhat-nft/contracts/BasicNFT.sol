4d2202b912c5911176a1d472494386c058e34d39
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 09:34:40 +0100

    add token URI

--- FCC 32 hrs course/hard hat/hardhat-nft/contracts/BasicNFT.sol
+++ FCC 32 hrs course/hard hat/hardhat-nft/contracts/BasicNFT.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNFT is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter;
 
     constructor() ERC721("Hamizzy", "HAM") {
@@ -14,6 +16,16 @@ contract BasicNFT is ERC721 {
     function mintNFT() public returns (uint256) {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter++;
+        return s_tokenCounter;
+    }
+
+    function tokenURI(
+        uint256 /*tokenId*/
+    ) public pure override returns (string memory) {
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
     }
 }
