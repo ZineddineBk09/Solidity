9fd01179041feceacc4ae1745beb35d31a052f3e
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 16:17:20 +0100

    add args

--- FCC 32 hrs course/hard hat/hardhat-nft/deploy/02-deploy-random-nft.js
+++ FCC 32 hrs course/hard hat/hardhat-nft/deploy/02-deploy-random-nft.js
@@ -23,12 +23,19 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   } else {
     // if we are in a public network (sepolia)
     vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
-    subscriptionId=networkConfig[chainId].subscriptionId
+    subscriptionId = networkConfig[chainId].subscriptionId
   }
 
   log(
-    '========================= Deploying Basic NFT ============================='
+    '========================= Deploying Random NFT ============================='
   )
 
-  const args = []
+  const args = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    networkConfig[chainId].gasLane,
+    networkConfig[chainId].callbackGasLimit,
+    // pupTokenUris,
+    networkConfig[chainId].mintFee,
+  ]
 }
