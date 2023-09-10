25ac72611f982191ead27c0eba360446b0ae73ae
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 15:30:18 +0100

    add subscription id for chainlink vrf

--- FCC 32 hrs course/hard hat/hardhat-nft/helper-hardhat-config.js
+++ FCC 32 hrs course/hard hat/hardhat-nft/helper-hardhat-config.js
@@ -16,7 +16,7 @@ const networkConfig = {
       '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c',
     callbackGasLimit: '500000', // 500,000 gas
     mintFee: '10000000000000000', // 0.01 ETH
-    subscriptionId: '1002', // add your ID here!
+    subscriptionId: '5124', // add your ID here!
   },
 }
 
