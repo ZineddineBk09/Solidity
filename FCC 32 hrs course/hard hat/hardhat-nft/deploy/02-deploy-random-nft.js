91862464d478f6e1be47af02b90f4d99c9fa8995
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 15:30:03 +0100

    random nft contract deploy script

---
+++ FCC 32 hrs course/hard hat/hardhat-nft/deploy/02-deploy-random-nft.js
@@ -0,0 +1,34 @@
+const { network, ethers } = require('hardhat')
+const { developmentChains, networkConfig } = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+
+  let vrfCoordinatorV2Address, subscriptionId
+
+  // if we are in localhost network or hardhat network
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      'VRFCoordinatorV2Mock'
+    )
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+
+    // create a subscription
+    const tx = await vrfCoordinatorV2Mock.createSubscription()
+    const txReceipt = await tx.wait(1)
+    subscriptionId = txReceipt.events[0].args.subId
+  } else {
+    // if we are in a public network (sepolia)
+    vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+    subscriptionId=networkConfig[chainId].subscriptionId
+  }
+
+  log(
+    '========================= Deploying Basic NFT ============================='
+  )
+
+  const args = []
+}
