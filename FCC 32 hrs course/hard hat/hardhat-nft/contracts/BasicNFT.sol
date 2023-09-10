f8758ef1814424f679e9c08fe849b8c9a93e3917
ZineddineBk09 <eino1209@gmail.com>
2023-09-10 09:26:47 +0100

    write the NFT mint function

--- FCC 32 hrs course/hard hat/hardhat-nft/contracts/BasicNFT.sol
+++ FCC 32 hrs course/hard hat/hardhat-nft/contracts/BasicNFT.sol
@@ -5,5 +5,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNFT is ERC721 {
-    constructor() ERC721("Hamizzy", "HAM") {}
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Hamizzy", "HAM") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+        return s_tokenCounter;
+    }
 }
