3af9c089aaac6c61f39782ccf60eda3d18f767b3
ZineddineBk09 <eino1209@gmail.com>
2023-09-11 11:11:45 +0100

    add solidity compiler v0.6

--- FCC 32 hrs course/hard hat/hardhat-nft/hardhat.config.js
+++ FCC 32 hrs course/hard hat/hardhat-nft/hardhat.config.js
@@ -21,12 +21,21 @@ module.exports = {
       chainId: 11155111,
       // blockConfirmations represents the number of blocks that need to be mined on top of the block that includes the transaction in order for the transaction to be considered confirmed.
       // we choose 6 because it is the default number of block confirmations for most exchanges
-      blockConfirmations: 6, 
+      blockConfirmations: 6,
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
   },
-  solidity: '0.8.19',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.19',
+      },
+      {
+        version: '0.6.6',
+      },
+    ],
+  },
   // we need namedAccounts for the deploy scripts, so we can use the deployer address in the scripts
   namedAccounts: {
     deployer: {
