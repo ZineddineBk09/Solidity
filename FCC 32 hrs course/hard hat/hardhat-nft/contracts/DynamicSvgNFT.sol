b3350fa8fba0a905a34871ede9bf690db8a5a637
ZineddineBk09 <eino1209@gmail.com>
2023-09-11 08:38:56 +0100

    encode svg images to base64

--- FCC 32 hrs course/hard hat/hardhat-nft/contracts/DynamicSvgNFT.sol
+++ FCC 32 hrs course/hard hat/hardhat-nft/contracts/DynamicSvgNFT.sol
@@ -6,8 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DynamicSvgNFT is ERC721 {
     uint256 private s_tokenCounter;
+    string[] private i_imagesUris;
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+xml;base64,";
 
-    constructor() ERC721("Monuments", "MNM") {}
+    constructor(string[] memory imagesUris) ERC721("Monuments", "MNM") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI() public pure returns (string memory) {}
 
     function mintNFT() public {
         _safeMint(msg.sender, s_tokenCounter);
